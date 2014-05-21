cc8d38f5311468576cda29018f061b5374ffbbb4
Nitin Reddy <k_nitin_r@yahoo.co.in>
2014-05-21 09:29:40 +0400

    Initial commit

---
+++ JobScheduler/JobSchedulerService.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace JobScheduler
+{
+    partial class JobSchedulerService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.process1 = new System.Diagnostics.Process();
+            this.tickTimer = new System.Timers.Timer();
+            ((System.ComponentModel.ISupportInitialize)(this.tickTimer)).BeginInit();
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            // 
+            // tickTimer
+            // 
+            this.tickTimer.Enabled = true;
+            this.tickTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.tickTimer_Elapsed);
+            // 
+            // JobSchedulerService
+            // 
+            this.ServiceName = "JobScheduler";
+            ((System.ComponentModel.ISupportInitialize)(this.tickTimer)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.Process process1;
+        private System.Timers.Timer tickTimer;
+    }
+}
---
+++ JobScheduler/JobSchedulerService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace JobScheduler
+{
+    public partial class JobSchedulerService : ServiceBase
+    {
+        public static Logger logger = LogManager.GetLogger("JobSchedulerService");
+
+        public JobSchedulerService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string processFilename = System.Configuration.ConfigurationManager.AppSettings["PathToRun"];
+            string tickSeconds = System.Configuration.ConfigurationManager.AppSettings["TickSeconds"];
+            int tickSecondsInt;
+            if (!int.TryParse(tickSeconds, out tickSecondsInt))
+            {
+                logger.Error("Could not parse tickSeconds configuration as a number: " + tickSeconds);
+            }
+            else
+            {
+                tickTimer.Interval = tickSecondsInt * 1000;
+                logger.Info("Setting the tick interval to " + tickSecondsInt.ToString());
+            }
+            //int.Parse(tickSeconds)
+            
+            logger.Info("The process filename is " + processFilename);
+            
+            process1.StartInfo.FileName = processFilename;
+            tickTimer.Start();
+
+            logger.Info("Scheduler service started");
+        }
+
+        protected override void OnStop()
+        {
+            tickTimer.Stop();
+
+            logger.Info("Scheduler service stopped");
+        }
+
+        private void tickTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            logger.Info("Timer tick occurred");
+            try
+            {
+                process1.Start();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+        }
+    }
+}
